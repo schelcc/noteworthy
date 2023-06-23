1dc0bdc9c62eaf3abe20337639c146684fb4d8cb
Cole Scheller <schelcc@pobox.com>
2023-06-23 13:11:45 -0400

    more theme options

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,20 @@
 use lazy_static::lazy_static;
 use tui::style::Color;
 
+use crate::intern_error;
+
 pub struct Theme {
     pub background: Color,
     pub foreground: Color,
+    pub success: Color,
+    pub alert: Color,
 }
 
 lazy_static! {
     pub static ref THEME: Theme = Theme {
-        background: Color::Rgb(40, 40, 40),
-        foreground: Color::Rgb(253, 244, 193),
+        background: Color::Black,
+        foreground: Color::White,
+        success: Color::Green,
+        alert: Color::Red,
     };
 }
