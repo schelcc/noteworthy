4ebba69a4cd4c7acc4540fe896f4d426f728ce26
Cole Scheller <schelcc@pobox.com>
2023-07-03 12:51:28 -0400

    licensing update, GPL notices added

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,15 @@
+/*
+This file is part of Noteworthy.
+
+Noteworthy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Noteworthy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Noteworthy. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use std::collections::HashMap;
 
 // #[macro_use]
@@ -11,6 +23,7 @@ pub struct Theme {
     pub foreground: Color,
     pub success: Color,
     pub alert: Color,
+    pub highlight: Color,
 }
 
 lazy_static! {
@@ -19,6 +32,7 @@ lazy_static! {
         foreground: Color::White,
         success: Color::Green,
         alert: Color::Red,
+        highlight: Color::Yellow,
     };
     pub static ref SETTINGS: HashMap<String, String> = {
         let mut output: HashMap<String, String> = HashMap::new();
--- src/intern_error.rs
+++ src/intern_error.rs
@@ -1,3 +1,15 @@
+/*
+This file is part of Noteworthy.
+
+Noteworthy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Noteworthy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Noteworthy. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("[ERR] Crossterm : {0}")]
